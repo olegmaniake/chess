2fb95d06c78feae88e904b6c26119caf716e3fc8
olegmaniake <olegmaniake@mail.ru>
2015-07-14 18:00:25 +0300

    new version of module & form for greate figure

---
+++ modules/chess_figure/includes/themes/chess_figure.tpl.php
@@ -0,0 +1,9 @@
+<?php
+function theme_chess_figure($variable){
+  $output = '<div class ="chess-figure-wrapper">';
+  $output .= '<div class = "body">';
+  $output .= '</div>';
+  $output .= '</div>';
+
+  return $output;
+}
