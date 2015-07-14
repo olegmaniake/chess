9a3a70a70575a23b9f525e36a5c15f2fb8176158
olegmaniake <olegmaniake@mail.ru>
2015-07-14 21:00:11 +0300

    dont work template

--- modules/chess_figure/includes/themes/chess_figure.tpl.php
+++ modules/chess_figure/includes/themes/chess_figure.tpl.php
@@ -1,7 +1,14 @@
 <?php
-function theme_chess_figure($variable){
+function theme_chess_figure_template($variable){
   $output = '<div class ="chess-figure-wrapper">';
   $output .= '<div class = "body">';
+  $flag_color = 'black';
+  for ($i=1;$i<=8;$i++){
+    for ($j=1;$j<=8;$j++){
+      $output .= '<div id ="'.$i.$j.'" color = "'.$flag_color.'"></div>';
+      $flag_color == 'black' ? $flag_color = 'white' : $flag_color = 'black';
+    }
+  }
   $output .= '</div>';
   $output .= '</div>';
 
